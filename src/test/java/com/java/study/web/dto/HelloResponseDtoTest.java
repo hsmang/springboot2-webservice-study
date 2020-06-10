b76cf8ac210c4132aadc24e1e88e9a0be79216d7
HanSangMin <smhan@simmssoft.com>
2020-06-10 17:42:06 +0900

    posts 클래스 까지 생성

---
+++ src/test/java/com/java/study/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,37 @@
+package com.java.study.web.dto;
+
+import com.java.study.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class HelloResponseDtoTest {
+
+
+
+    @Test
+    public void 롬복_기능_테스트() throws Exception {
+        //given
+
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+}
